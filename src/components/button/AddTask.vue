b4558008a906e56ef49745428e4fa09d1967da30
Laysa <laysa.developer@gmail.com>
2024-07-08 11:42:28 -0300

    feat: add primevue button component

---
+++ src/components/button/AddTask.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import Button from 'primevue/button';
+
+defineProps<{
+  title: string;
+}>()
+</script>
+
+<template>
+    <Button severity="success" class="uppercase font-bold">{{ title }}</Button>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import './assets/css/main.css'
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Lara from '@primevue/themes/lara';
+import './assets/css/main.css';
+import App from './App.vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+createApp(App)
+.use(PrimeVue, {
+    theme: {
+        preset: Lara
+    }
+})
+.mount('#app');
