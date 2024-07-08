9710a58cac407d72d8faff770f9a3688c74687a3
Laysa <laysa.developer@gmail.com>
2024-07-08 19:12:59 -0300

    feat: create initial main screen

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
+<script setup>
+import AddTask from './components/button/AddTask.vue';
+import TaskInput from './components/input/TaskInput.vue';
+</script>
+
 <template>
-  <h1>TO DO LIST APP with VueJS</h1>
+  <main class="bg-green-500 min-h-screen flex items-center justify-center flex-col p-20">
+      <h1 class="text-2xl mb-8 font-bold">TO DO LIST APP with VueJS</h1>
+      <TaskInput/>
+      <div class="pt-4">
+          <AddTask title="create"/>
+      </div>
+  </main>
 </template>
